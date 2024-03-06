d2ab50a985ce700d6337de48859a10bd36c847da
Yashraj <yashtam1@gmail.com>
2024-03-06 23:10:20 +0530

    Second Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world! Yashraj");
+        System.out.println("Hello Github! 2 nd commit");
     }
 }
